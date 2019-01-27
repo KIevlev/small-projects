1a1a4e1ea1cf2b30a1c41b2f5ea12fd8e39942db
KIevlev <Kika.Ievlev@yandex.ru>
2019-01-27 14:13:05 +0300

    1 new project

---
+++ validation/va.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+char str[7];
+int i;
+int flag=1;
+puts("Enter string with 6 digits without spaces");
+gets(str);
+for (i = 0; i<6;i++)
+{
+if(!isdigit(str[i]))
+{
+flag = 0;
+if(isalpha(str[i]))
+{printf("Letter found (%c)", str[i]);}
+else if (ispunct(str[i]))
+{printf("punctuation found");}
+else if (isspace(str[i]))
+{printf("space found");}
+}
+}
+(flag)? puts("\nEntry valid"):puts("\nEntry invalid");
+return 0;
+}
+
